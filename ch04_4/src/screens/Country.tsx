5a192ba52e034f0b3ea217e6b349b8febb0ee837
nogal <shtmdgus@gmail.com>
2022-03-14 19:25:15 +0900

    Fetch

--- ch04_4/src/screens/Country.tsx
+++ ch04_4/src/screens/Country.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { FC } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Colors } from "react-native-paper";
+import * as D from '../data'
 
-const title = 'Country'
 
-export default function Country() {
+export type CountryProps = {
+    country: D.ICountry
+}
+
+const Country:FC<CountryProps> = ({country}) => {
+    const {name, capital, population, subregion, region} = country
+    console.log('country')
+
     return (
         <View style={styles.view}>
-            <Text style={styles.text}>{title}</Text>
+            <View>
+                <Text style={styles.name}>{name}</Text>
+            </View>
+            <View>
+                <Text>capital: {capital}</Text>
+                <Text>population: {population}</Text>
+                <Text>subregion: {subregion}</Text>
+                <Text>region: {region}</Text>
+            </View>
         </View>
     )
 }
-
+export default Country
 const styles = StyleSheet.create ({
-    view: {flex:1, padding:5, backgroundColor: Colors.blue900},
-    text: {fontSize: 20, color: 'white'}
+    view: {padding:5},
+    name: {fontSize: 30, fontWeight:'400'},
+    
 })
